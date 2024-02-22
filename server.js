b9f4a8ecae60fcba87284eaed7665c30ad06cb74
Reydel Gutierrez <reydelworks@gmail.com>
2024-02-21 23:27:01 -0500

    test

--- server.js
+++ server.js
@@ -407,9 +407,6 @@ app.get('/fetchPunchRequest', async (req, res) => {
 });
 
   //rendering home page
-  app.get('/home', (req, res) => {
-    res.sendFile(path.join(__dirname, 'static', 'Web', 'home.html'));
-  });
 
   app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on http://localhost:${PORT}`);
